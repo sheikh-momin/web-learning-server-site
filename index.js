fd0e241851f4e1757d4a2a07e98e353d7c69d107
sheikh-momin <momin.emon14@gmail.com>
2022-10-25 13:47:41 -0700

    add corses picture

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port =process.env.PORT || 5000
 
 app.use(cors())
 const categories = require('./data/categories.json')
+const courses =require('./data/courses.json')
 
 app.get('/', (req, res)=>{
   res.send('Api running')
@@ -12,6 +13,17 @@ app.get('/', (req, res)=>{
 
 app.get('/web-categories', (req, res)=>{
   res.send(categories)
+})
+
+app.get('/course', (req, res) =>{
+  res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id
+  const selectedCourse = courses.find(c => c.category_id === id)
+  console.log(selectedCourse)
+  res.send(selectedCourse)
 })
 
 app.listen(port, () =>{
